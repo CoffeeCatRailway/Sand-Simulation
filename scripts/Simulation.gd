f836d5e7260909ee8e27e349cdec50c166a7a022
CoffeeCatRailway <dragonlee1002@gmail.com>
2024-01-25 12:07:25 +1100

    Simple brush settings & gas

--- scripts/Simulation.gd
+++ scripts/Simulation.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 @onready var colorRect: ColorRect = $CanvasLayer/Control/ColorRect
 @export var cellSize: int = 10
+@export var brushRadius: int = 3
+@export var squareBrush: bool = true
 
 var width: int
 var height: int
@@ -18,31 +20,54 @@ func _ready() -> void:
 	
 	data.resize(width * height)
 	data.fill(0)
-	data[(height / 2) * height + (width / 2)] = 1 	# sand
-	data[0 * height + 0] = 2						# red
-	data[0 * height + (width - 1)] = 3				# green
-	data[(height - 2) * height + 0] = 4				# blue
+	#data[(height / 2) * height + (width / 2)] = 1 	# sand
+	#data[0 * height + 0] = 2						# gas
+	#data[0 * height + (width - 1)] = 3				# green
+	#data[(height - 2) * height + 0] = 4				# blue
+	
+	for x in range(0, width):
+		for y in range(0, height):
+			var i = y * height + x
+			if x == 0 || y == 0 || x == width - 1 || y == height - 1:
+				data[i] = 4
+			#elif x % 2 == 0 && y % 2 == 0:
+				#data[i] = 2
+			#else:
+				#data[i] = 3
 	
 	passToShader()
 
 func _process(delta) -> void:
+	var tempPos := Vector2i.ZERO
 	if Input.is_action_pressed("mouse_left"):
-		var mpos := Vector2i(get_local_mouse_position()) / cellSize
-		for x in range(-1, 2):
-			for y in range(-1, 2):
-				var i = (mpos.y + y) * height + (mpos.x + x)
-				if data[i] == 0:
-					data[i] = 1
-					markUpdate = true
+		var element := 1
+		if Input.is_action_pressed("ui_home"):
+			element = 2
+		
+		var mPos := Vector2i(get_local_mouse_position()) / cellSize
+		for x in range(-brushRadius, brushRadius + 1):
+			for y in range(-brushRadius, brushRadius + 1):
+				tempPos.x = mPos.x + x
+				tempPos.y = mPos.y + y
+				if vec2iDist(mPos, tempPos) <= brushRadius || squareBrush:
+					var i = tempPos.y * height + tempPos.x
+					if data[i] == 0:
+						data[i] = element
+						markUpdate = true
 	if Input.is_action_pressed("mouse_right"):
-		var mpos := Vector2i(get_local_mouse_position()) / cellSize
-		print(mpos)
-		for x in range(-1, 2):
-			for y in range(-1, 2):
-				var i = (mpos.y + y) * height + (mpos.x + x)
-				if data[i] != 0:
-					data[i] = 0
-					markUpdate = true
+		var mPos := Vector2i(get_local_mouse_position()) / cellSize
+		for x in range(-brushRadius, brushRadius + 1):
+			for y in range(-brushRadius, brushRadius + 1):
+				tempPos.x = mPos.x + x
+				tempPos.y = mPos.y + y
+				if vec2iDist(mPos, tempPos) <= brushRadius || squareBrush:
+					var i = tempPos.y * height + tempPos.x
+					if data[i] != 0:
+						data[i] = 0
+						markUpdate = true
+
+func vec2iDist(a: Vector2i, b: Vector2i) -> float:
+	return sqrt(pow(a.x - b.x, 2.) + pow(a.y - b.y, 2.))
 
 func _physics_process(delta) -> void:
 	simulate()
@@ -67,6 +92,13 @@ func simulate() -> void:
 						data[i] = 0
 						data[dy * height + dx] = 1
 						markUpdate = true
+			elif data[i] == 2:
+				var dx: int = clamp(x + (1 if randf() > .5 else -1), 0, width - 1)
+				var dy: int = clamp(y + (1 if randf() > .5 else -1), 0, height - 1)
+				if data[dy * height + dx] == 0:
+					data[i] = 0
+					data[dy * height + dx] = 2
+					markUpdate = true
 
 func passToShader() -> void:
 	var image := Image.create(width, height, false, Image.FORMAT_RGB8)
@@ -76,7 +108,7 @@ func passToShader() -> void:
 			if data[i] == 1:
 				image.set_pixel(x, y, Color.SANDY_BROWN)
 			elif data[i] == 2:
-				image.set_pixel(x, y, Color.RED)
+				image.set_pixel(x, y, Color.LIGHT_GRAY)
 			elif data[i] == 3:
 				image.set_pixel(x, y, Color.GREEN)
 			elif data[i] == 4:
