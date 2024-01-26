c0515b7c76d53852d3347a8cdf18de37dcf551aa
CoffeeCatRailway <dragonlee1002@gmail.com>
2024-01-26 13:26:19 +1100

    feckin borked!

--- scripts/Simulation.gd
+++ scripts/Simulation.gd
@@ -8,40 +8,50 @@ extends Node2D
 var width: int
 var height: int
 
-var data: Dictionary = {} # Vector2i/Cell
+var data: Array[Array] = []
 var markUpdate := false
 
 func _ready() -> void:
+	print("Brush radius: ", brushRadius)
+	print("Brush type: ", ("Square" if squareBrush else "Circle"))
+	
+	# Calculate width/height
 	var control := Vector2i($CanvasLayer/Control.size)
 	width = control.x / cellSize
 	height = control.y / cellSize
 	print("Sim Resolution: %s/%s" % [width, height])
+	# Pass width/height to sahder
 	colorRect.material.set_shader_parameter("size", Vector2(width, height))
 	
-	#data.resize(width * height)
-	#data.fill(0)
-	#data[(height / 2) * height + (width / 2)] = 1 	# sand
-	#data[0 * height + 0] = 2						# gas
-	#data[0 * height + (width - 1)] = 3				# green
-	#data[(height - 2) * height + 0] = 4				# blue
+	# Fill array(s)
+	var yarr: Array[int] = []
+	yarr.resize(height)
+	yarr.fill(0)
+	data.resize(width)
+	data.fill(yarr)
+	#print(data.size(), "/", yarr.size())
 	
-	var pos := Vector2i.ZERO
-	for x in range(0, width):
-		for y in range(0, height):
-			pos.x = x
-			pos.y = y
+	#data[width / 2][height / 2] = 1 	# sand
+	#data[0][0] = 2						# gas
+	#data[width - 1][0] = 3				# green
+	#data[0][height - 2] = 4				# blue
+	
+	for x in range(0, width - 1):
+		for y in range(0, height - 1):
 			if x == 0 || y == 0 || x == width - 1 || y == height - 1:
-				setCellv(pos, 4)
+				setCell(x, y, 4)
 	
+	# Initialze screen
 	passToShader()
 
 func _process(delta) -> void:
-	var tempPos := Vector2i.ZERO
 	var mouseLeft := Input.is_action_pressed("mouse_left")
 	var mouseRight := Input.is_action_pressed("mouse_right")
 	
 	if mouseLeft || mouseRight:
+		var tempPos := Vector2i.ZERO
 		var mPos := Vector2i(get_local_mouse_position()) / cellSize
+		#print("Mouse pos: ", mPos)
 		for x in range(-brushRadius, brushRadius + 1):
 			for y in range(-brushRadius, brushRadius + 1):
 				tempPos.x = mPos.x + x
@@ -52,97 +62,83 @@ func _process(delta) -> void:
 						if Input.is_action_pressed("ui_home"):
 							element = 2
 						if getCellv(tempPos) == 0:
-							setCellv(tempPos, element)
+							setCell(x, y, element)
 							markUpdate = true
 					if mouseRight:
-						eraseCell(tempPos)
+						setCell(x, y, 0)
 						markUpdate = true
 
 func getCell(x: int, y: int) -> int:
 	return getCellv(Vector2i(x, y))
 
 func getCellv(pos: Vector2i) -> int:
-	var p := Vector2i.ZERO
-	p.x = clamp(pos.x, 0, width - 1)
-	p.y = clamp(pos.y, 0, height - 1)
-	return data.get(p, 0)
+	var x := clampi(pos.x, 0, width - 1)
+	var y := clampi(pos.y, 0, height - 1)
+	return data[x][y]
 
 func setCell(x: int, y: int, cell: int) -> int:
 	return setCellv(Vector2i(x, y), cell)
 
 func setCellv(pos: Vector2i, cell: int) -> int:
-	var p := Vector2i.ZERO
-	p.x = clamp(pos.x, 0, width - 1)
-	p.y = clamp(pos.y, 0, height - 1)
+	var x := clampi(pos.x, 0, width - 1)
+	var y := clampi(pos.y, 0, height - 1)
 	
-	var old = data.get(p, 0)
-	data[p] = cell
+	var old := getCell(x, y)
+	data[x][y] = cell
 	return old
-
-func eraseCell(pos: Vector2i) -> bool:
-	return data.erase(pos)
 
 func vec2iDist(a: Vector2i, b: Vector2i) -> float:
 	return sqrt(pow(a.x - b.x, 2.) + pow(a.y - b.y, 2.))
 
 func _physics_process(delta) -> void:
-	if markUpdate:
-		sortCells()
-	
 	simulate()
 	
 	if markUpdate:
 		passToShader()
 
-func sortCells() -> void:
-	var newCells: Dictionary = {}
-	var keys = data.keys()
-	keys.sort_custom(func(a, b): return a.y < b.y)
-	for pos in keys:
-		newCells[pos] = data[pos]
-	data = Dictionary(newCells)
-
 func simulate() -> void:
-	for pos in data:
-		var cell = data[pos]
-		if cell != 0 && cell != 1:
-			continue
-		
-		if cell == 1: # sand
-			if pos.y != 0:
-				var dx: int = clamp(pos.x + (1 if randf() > .5 else -1), 0, width - 1)
-				var dy: int = clamp(pos.y - 1, 0, height - 1)
-				
-				if getCell(pos.x, dy) == 0:
-					eraseCell(pos)
-					setCell(pos.x, dy, 1)
-					markUpdate = true
-				elif getCell(dx, dy) == 0:
-					eraseCell(pos)
-					setCell(dx, dy, 1)
-					markUpdate = true
-		elif cell == 2: # gas
-			var dx: int = clamp(pos.x + (1 if randf() > .5 else -1), 0, width - 1)
-			var dy: int = clamp(pos.y + (1 if randf() > .5 else -1), 0, height - 1)
+	for x in range(0, width - 1): # TODO: Shuffle x indecies & loop y,x
+		for y in range(0, height - 1):
+			var cell := getCell(x, y)
+			if cell != 0 && cell != 1:
+				continue
 			
-			if getCell(dx, dy) == 0:
-				eraseCell(pos)
-				setCell(dx, dy, 2)
-				markUpdate = true
+			if cell == 1: # sand
+				if y != 0:
+					var dx: int = clamp(x + (1 if randf() > .5 else -1), 0, width - 1)
+					var dy: int = clamp(y - 1, 0, height - 1)
+					
+					if getCell(x, dy) == 0:
+						setCell(x, y, 0)
+						setCell(x, dy, 1)
+						markUpdate = true
+					elif getCell(dx, dy) == 0:
+						setCell(x, y, 0)
+						setCell(dx, dy, 1)
+						markUpdate = true
+			elif cell == 2: # gas
+				var dx: int = clamp(x + (1 if randf() > .5 else -1), 0, width - 1)
+				var dy: int = clamp(y + (1 if randf() > .5 else -1), 0, height - 1)
+				
+				if getCell(dx, dy) == 0:
+					setCell(x, y, 0)
+					setCell(dx, dy, 2)
+					markUpdate = true
 
 func passToShader() -> void:
 	var image := Image.create(width, height, false, Image.FORMAT_RGB8)
 	image.fill(Color.BLACK)
-	for pos in data:
-		var cell = data[pos]
-		if cell == 1:
-			image.set_pixelv(pos, Color.SANDY_BROWN)
-		elif cell == 2:
-			image.set_pixelv(pos, Color.LIGHT_GRAY)
-		elif cell == 3:
-			image.set_pixelv(pos, Color.GREEN)
-		elif cell == 4:
-			image.set_pixelv(pos, Color.BLUE)
+	for x in range(0, width - 1):
+		for y in range(0, height - 1):
+			var cell := getCell(x, y)
+			if cell == 1:
+				image.set_pixel(x, y, Color.SANDY_BROWN)
+			elif cell == 2:
+				image.set_pixel(x, y, Color.LIGHT_GRAY)
+			elif cell == 3:
+				image.set_pixel(x, y, Color.GREEN)
+			elif cell == 4:
+				image.set_pixel(x, y, Color.BLUE)
 	
 	var texture = ImageTexture.create_from_image(image)
 	colorRect.material.set_shader_parameter("tex", texture)
